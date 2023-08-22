88d138a05f8a1cb3bd3939a8021a746655fe7508
Jeff Zellman <jzellman@gmail.com>
2023-08-22 17:34:02 -0500

    offload Redis commands until after initilization to prevent crashing the supervisor
    prior to these changes, if Redis was unavailable during startup
    of an Elixir application or if Redis becomes unavailable during
    running, the some commands executed would crash the supervisor and thus
    crash the application. The following changes where made:
    
     - remove checking the Redis version - Just assume Redis is setup
       correctly
     - move group creation into heartbeat. use similar looping as heartbeat
       check. Call it before heartbeat in handle_continue.

--- lib/heartbeat.ex
+++ lib/heartbeat.ex
@@ -22,6 +22,7 @@ defmodule OffBroadwayRedisStream.Heartbeat do
 
   @impl true
   def handle_continue(nil, state) do
+    create_group(state)
     heartbeat(state)
     {:noreply, state}
   end
@@ -34,6 +35,25 @@ defmodule OffBroadwayRedisStream.Heartbeat do
 
   @redis_command_retry_timeout 300
   @max_retries 2
+
+  defp create_group(%{client: client, config: config} = state, retry_count \\ 0) do
+    # we need to ensure the group is created before a heartbeat will succeed
+    case client.create_group(config[:group_start_id], config) do
+      :ok ->
+        :ok
+
+      {:error, %RedisClient.ConnectionError{} = error} when retry_count < @max_retries ->
+        Logger.warn(
+          "Failed to create group, retry_count: #{retry_count}, reason: #{inspect(error.reason)}"
+        )
+
+        Process.sleep(@redis_command_retry_timeout * (retry_count + 1))
+        create_group(state, retry_count + 1)
+
+      {:error, error} ->
+        raise "Create group failed, " <> inspect(error)
+    end
+  end
 
   defp heartbeat(
          %{client: client, config: config, heartbeat_interval: interval} = state,
